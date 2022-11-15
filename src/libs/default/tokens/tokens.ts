77b7cc718052213bb9b35903ffcfbe590a8ed980
RahulNavneeth <rahulmnavneeth@gmail.com>
2022-11-15 19:09:38 +0530

    update - drop

--- src/libs/default/tokens/tokens.ts
+++ src/libs/default/tokens/tokens.ts
@@ -1,9 +1,10 @@
 import { writable } from 'svelte/store';
 
-export const TOKEN_GRID = writable<boolean[][]>([
-	[true, false, true, true, true],
-	[true, true, false, true, true],
-	[true, false, false, true, true],
-	[true, false, true, true, true],
-	[true, true, false, true, true]
-]);
+export const TOKEN_GRID = writable<boolean[][]>(
+	// [true, false, true, true, true],
+	// [true, true, false, true, true],
+	// [true, false, false, true, true],
+	// [true, false, true, true, true],
+	// [true, true, false, true, true]
+	[...Array(6)].map((j) => [...Array(5)].map((i) => (Math.floor(Math.random() * 2) ? true : false)))
+);
--- src/libs/functions/drop.ts
+++ src/libs/functions/drop.ts
@@ -6,17 +6,24 @@ TOKEN_GRID.subscribe((i) => {
 });
 export const drop = () => {
 	const TOKEN_LEN = TOKEN.length - 1;
-	for (let i = 0; i < TOKEN.length; i++) {
-		for (let j = 0; j < TOKEN[i].length; j++) {
-			console.log(TOKEN_LEN - i, j);
-			if (TOKEN_LEN - i < TOKEN_LEN) {
-				if (TOKEN[TOKEN_LEN - i][j] && !TOKEN[TOKEN_LEN - i + 1][j]) {
-					TOKEN[TOKEN_LEN - i][j] = false;
-					TOKEN[TOKEN_LEN - i + 1][j] = true;
-					TOKEN_GRID.set(TOKEN);
+	// let f_count = 0;
+
+	// for (let x = 0; x < TOKEN.length; x++) {
+	// 	f_count += TOKEN[x].filter((i) => !i).length;
+	// }
+	setInterval(() => {
+		for (let i = 0; i < TOKEN.length; i++) {
+			for (let j = 0; j < TOKEN[i].length; j++) {
+				console.log(TOKEN_LEN - i, j);
+				if (TOKEN_LEN - i < TOKEN_LEN) {
+					if (TOKEN[TOKEN_LEN - i][j] && !TOKEN[TOKEN_LEN - i + 1][j]) {
+						TOKEN[TOKEN_LEN - i][j] = false;
+						TOKEN[TOKEN_LEN - i + 1][j] = true;
+						TOKEN_GRID.set(TOKEN);
+					}
 				}
 			}
 		}
-	}
+	}, 100);
 	console.log(TOKEN);
 };
